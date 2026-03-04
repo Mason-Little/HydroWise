68d692e3a071d8237ea7ddc7f0722ac91531af89
Mason <mason@scalar.com>
2026-03-03 22:17:01 -0800

    feat: course schema

--- packages/entities/src/course/course.schema.ts
+++ packages/entities/src/course/course.schema.ts
@@ -1,8 +1,29 @@
 import { z } from "zod";
 
+const GradeRubricItemSchema = z.object({
+  category: z.string(),
+  weight: z.number(),
+});
+
+const TestDateSchema = z.object({
+  name: z.string(),
+  date: z.string(),
+});
+
+const ProfessorInformationSchema = z.object({
+  professorName: z.string(),
+  professorEmail: z.string(),
+  professorOfficeHours: z.string(),
+  professorOfficeDays: z.string(),
+});
+
 export const CourseSchema = z.object({
   id: z.string().min(1),
-  name: z.string(),
+  courseName: z.string(),
+  courseCode: z.string(),
+  gradeRubric: z.array(GradeRubricItemSchema),
+  testDates: z.array(TestDateSchema),
+  professorInformation: ProfessorInformationSchema,
 });
 
 export type Course = z.infer<typeof CourseSchema>;
